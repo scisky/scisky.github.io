43551412c2b7cc143dcc3185c93c2db6b236e31f
skyhoffert <skwylarh@yahoo.com>
2019-09-26 15:39:13 -0400

    Added a page about converting from a uniform to a normal distribution.

---
+++ distribs.js
@@ -0,0 +1,207 @@
+// distribs
+// Last modified September 26, 2019
+
+const UPDATE_RATE = 1000/60;
+const BGCOLOR = "#aaaaaa";
+
+var fontSize = 30;
+
+// Grab the canvas from the html document and set things for it
+var canvas = document.getElementById("main_canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+ctx.font = "" + fontSize + "px Verdana";
+ctx.fillStyle = BGCOLOR;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+var lmousedown = false;
+var lmouselastx = 0;
+var lmouselasty = 0;
+var lmousecurx = 0;
+var lmousecury = 0;
+
+var keys = new Array(1000).fill(0);
+
+var uniMin = 0;
+var uniMax = 1;
+var uniBins = 60;
+var uniHisto = new Array(uniBins).fill(0);
+var uniMaxVal = 0;
+
+var gMin = -5;
+var gMax = 5;
+var gBins = 60;
+var gHisto = new Array(gBins).fill(0);
+var gMaxVal = 0;
+
+var counterMax = 1;
+var counter = counterMax;
+
+setInterval(update, UPDATE_RATE);
+
+canvas.addEventListener("mousedown", function(evt)
+{
+    lmousedown = true;
+    lmouselastx = evt.x;
+    lmouselasty = evt.y;
+    lmousecurx = evt.x;
+    lmousecury = evt.y;
+}, false);
+
+canvas.addEventListener("mousemove", function(evt)
+{
+    // If the user is "dragging".
+    if (lmousedown)
+    {
+        lmousecurx = evt.x;
+        lmousecury = evt.y;
+    }
+    else
+    {
+        lmouselastx = evt.x;
+        lmouselasty = evt.y;
+    }
+}, false);
+
+canvas.addEventListener("mouseup", function(evt)
+{
+    lmousedown = false;
+}, false);
+
+canvas.addEventListener("mousewheel", function(evt)
+{
+    var delta = Math.max(-1, Math.min(1, (evt.wheelDelta || -evt.detail)));
+
+    if (delta < 0)
+    {
+    }
+    else
+    {
+    }
+}, false);
+
+document.body.onkeydown = function(e)
+{
+    keys[e.keyCode] = true;
+}
+
+document.body.onkeyup = function(e)
+{
+    keys[e.keyCode] = false;
+}
+
+function DrawRandomNumbers()
+{
+    let randVal = Math.random();
+    let gIndex = (randVal - 0.5) * Math.PI;
+    let gVal = Math.tan(gIndex);
+    
+    ctx.fillStyle = "black";
+    ctx.fillText("Uniform:", 100, 170);
+    ctx.fillText("" + randVal, 100, 200);
+    
+    ctx.fillText("Gaussian Index:", 100, 270);
+    ctx.fillText("" + gIndex, 100, 300);
+    
+    ctx.fillText("Gaussian:", 100, 370);
+    ctx.fillText("" + gVal, 100, 400);
+    ctx.fillText("Gaussian Index = (Uniform - 0.5) * \u03D6", 100, 600);
+    ctx.fillText("Gaussian = Tangent(Gaussian Index)", 100, 640);
+    
+    ctx.fillText("Uniform Distribution", canvas.width/2, 70);
+    ctx.fillText("Gaussian/Normal Distribution", canvas.width/2, 370);
+    
+    // Add to Uniform Histogram
+    let uniStep = (uniMax - uniMin)/uniBins;
+    for (let i = 0; i < uniBins; i++)
+    {
+        if (randVal > (uniMin + uniStep*i) && randVal < (uniMin + uniStep*(i+1)))
+        {
+            uniHisto[i]++;
+            
+            if (uniHisto[i] > uniMaxVal)
+            {
+                uniMaxVal = uniHisto[i];
+            }
+        }
+        
+        ctx.fillStyle = "black";
+        ctx.fillRect(canvas.width/2 + i*10, 200, 1, 2);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(canvas.width/2 + i*10, 200, 10, -100*(uniHisto[i]/uniMaxVal));
+    }
+    
+    ctx.strokeStyle = "black";
+    ctx.setLineDash([5, 5]);/*dashes are 5px and spaces are 3px*/
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 100);
+    ctx.lineTo(canvas.width/2 + uniBins*10, 100);
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.fillText("" + uniMaxVal, canvas.width/2 - 60, 110);
+    ctx.fillText("" + uniMin, canvas.width/2 - 10, 230);
+    ctx.fillText("" + uniMax, canvas.width/2 + uniBins*10 - 20, 230);
+    
+    // Add to Gaussian Histogram
+    let gStep = (gMax - gMin)/gBins;
+    for (let i = 0; i < gBins; i++)
+    {
+        if (gVal > (gMin + gStep*i) && gVal < (gMin + gStep*(i+1)))
+        {
+            gHisto[i]++;
+            
+            if (gHisto[i] > gMaxVal)
+            {
+                gMaxVal = gHisto[i];
+            }
+        }
+        
+        ctx.fillStyle = "black";
+        ctx.fillRect(canvas.width/2 + i*10, 500, 1, 2);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(canvas.width/2 + i*10, 500, 10, -100*(gHisto[i]/gMaxVal));
+    }
+    
+    ctx.strokeStyle = "black";
+    ctx.setLineDash([5, 5]);/*dashes are 5px and spaces are 3px*/
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 400);
+    ctx.lineTo(canvas.width/2 + gBins*10, 400);
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.fillText("" + gMaxVal, canvas.width/2 - 60, 410);
+    ctx.fillText("" + gMin, canvas.width/2 - 10, 530);
+    ctx.fillText("" + gMax, canvas.width/2 + gBins*10 - 30, 530);
+}
+
+function update()
+{
+    if (counter == counterMax)
+    {
+        if (!keys[32])
+        {
+            ctx.fillStyle = BGCOLOR;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            
+            ctx.fillStyle = "black";
+            ctx.fillText("Hold Space Bar to pause", 80, 40);
+            
+            ctx.fillStyle = "green";
+            ctx.fillRect(10, 10, 40, 40);
+            
+            DrawRandomNumbers();
+        }
+        else
+        {
+            ctx.fillStyle = "red";
+            ctx.fillRect(10, 10, 40, 40);
+        }
+        
+        counter = 0;
+    }
+    else
+    {
+        counter++;
+    }
+}
